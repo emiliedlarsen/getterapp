e38c8896685d8fe3eafdca65cb957f60e47ff513
simonjuha <33484974+simonjuha@users.noreply.github.com>
2018-05-22 12:37:28 +0200

    formatering

--- script.js
+++ script.js
@@ -1,79 +1,81 @@
 $(".page, .overlay").hide(); //skjul elementer
 
-
-
-$( document ).ready(function() {
+$(document).ready(function () {
 	"use strict";
 	$("#frontpage").show(); //vis forsiden
 
 	// link til anden side
-	$("[pageDirect]").css("cursor","pointer"); //links får en pointer cursor
-	$("[pageDirect]").click(function() {
-        $(".page").hide(); //alle sider skjules
-	    $("#"+$(this).attr("pageDirect")).show(); 
+	$("[pageDirect]").css("cursor", "pointer"); //links får en pointer cursor
+	$("[pageDirect]").click(function () {
+		$(".page").hide(); //alle sider skjules
+		$("#" + $(this).attr("pageDirect")).show();
 		// navnet på den ID som er i attributen "pageDirect" vises
 		// "pageDirect" fungerer som et link til et ID af en ".page"
 	});
-	
-// post generator
-// t: overskrift c: kategori a: adresse ti: tid
- function createListEl(t,c,a,ti,date) {$( "#postlist .post-desc, #profile-posts .post-desc" ).after("<div class='row new-post-row'><div class='col-2-sm'>"+t+"</div><div class='col-3-sm'>"+c+"</div><div class='col-3-sm'>"+a+"</div><div class='col-2-sm'>"+ti+"</div><div class='col-2-sm'>"+date+"</div></div></div>");}	
 
-createListEl("Tømrer","handyman","gadenavn 24","12:00","29/4");
-createListEl("Røvin","Mad og drikke","vejnavn 48","21:00","17/5");
+	// post generator
+	// t: overskrift c: kategori a: adresse ti: tid
+	function createListEl(t, c, a, ti, date) {
+		$("#postlist .post-desc, #profile-posts .post-desc").after("<div class='row new-post-row'><div class='col-2-sm'>" + t + "</div><div class='col-3-sm'>" + c + "</div><div class='col-3-sm'>" + a + "</div><div class='col-2-sm'>" + ti + "</div><div class='col-2-sm'>" + date + "</div></div></div>");
+	}
+
+	createListEl("Tømrer", "handyman", "gadenavn 24", "12:00", "29/4");
+	createListEl("Rødvin", "Mad og drikke", "vejnavn 48", "21:00", "17/5");
+
 
 	
-$("[pageDirect]").css("cursor","pointer"); //links får en 
 
+
+	$("[icon]").addClass("icon"); // klassen "icon" tilføjes til elementer med attr "icon"
+	$("[icon]").each(function () {
+		var ic = $(this).attr("icon"); // fil navn gemmes
+		$(this).css("background-image", "url(icons/" + ic + ")"); // background på icon skiftes
+	});
 	
-$("[icon]").addClass("icon"); // klassen "icon" tilføjes til elementer med attr "icon"
-$("[icon]").each(function() {
-	var ic = $(this).attr("icon"); // fil navn gemmes
-	$(this).css("background-image", "url(icons/"+ic+")"); // background på icon skiftes
-});	
 	//Jquery UI
 	$("#profile-tabs").tabs();
 	$("button").button();
-	$( "input[type='checkbox']" ).checkboxradio();
-	var newTime = $( "#new-time" ).spinner({
-		min:0,
-		max:24
-	}).spinner( "value", 12 );
-    $( "#check-time" ).on( "click", function() {
-      if ( newTime.spinner( "option", "disabled" ) ) {
-        newTime.spinner( "enable" );
-      } else {
-        newTime.spinner( "disable" );
-      }
-    });
+	$("input[type='checkbox']").checkboxradio();
+	var newTime = $("#new-time").spinner({
+		min: 0,
+		max: 24
+	}).spinner("value", 12);
+	$("#check-time").on("click", function () {
+		if (newTime.spinner("option", "disabled")) {
+			newTime.spinner("enable");
+		} else {
+			newTime.spinner("disable");
+		}
+	});
+	
 	$("#form-category").selectmenu();
-	
 
-$( ".label" ).hover( //hover over label
-  function() {
-var overlay = $(this).find(".overlay"); // .overlay klasse i .label gemmes
-	   if ( $( overlay ).is( ":hidden" ) ) { // hvis overlay er skjult?
-    $( overlay ).slideDown( "slow" ); // slide down overlay
-  } else { 
-    $( overlay ).hide(); // ellers skjul
-  }}).mouseleave(function() {
-    $(this).find(".overlay").hide(); // fikser at overlay bliver nede når der skiftes side da siden også skjules.
-});
-	
-// slå opslag op
-	function submitPost(){
+
+	$(".label").hover( //hover over label
+		function () {
+			var overlay = $(this).find(".overlay"); // .overlay klasse i .label gemmes
+			if ($(overlay).is(":hidden")) { // hvis overlay er skjult?
+				$(overlay).slideDown("slow"); // slide down overlay
+			} else {
+				$(overlay).hide(); // ellers skjul
+			}
+		}).mouseleave(function () {
+		$(this).find(".overlay").hide(); // fikser at overlay bliver nede når der skiftes side da siden også skjules.
+	});
+
+	// slå opslag op
+	function submitPost() {
 		var t = $('#form-title').val();
 		var c = $('#form-category').val();
 		var a = $('#form-adress').val();
 		var ti = $('#new-time').val();
 		var date = new Date(); // ny dato
-		var h   = date.getDate(); // få dato
-  		var m = date.getMonth(); // måned
-		createListEl(t,c,a,ti,h+"/"+m); // der sinsættes en psot med createListEL()
+		var h = date.getDate(); // få dato
+		var m = date.getMonth(); // måned
+		createListEl(t, c, a, ti, h + "/" + m); // der sinsættes en psot med createListEL()
 	}
- 	$("#submitnewpost").click(function() {submitPost();}); // submit post fyres afsted når der trykkes på knappen
+	$("#submitnewpost").click(function () {
+		submitPost();
+	}); // submit post fyres afsted når der trykkes på knappen
 
 });
-
-
-
