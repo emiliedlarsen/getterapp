1ed3bf7b9124991c05ceb71cde4a2404fcfcf832
simonjuha <simonjuha@users.noreply.github.com>
2018-05-23 15:36:35 +0200

    posts
    post kan nu åbnes
    beskrivelse følger med

--- script.js
+++ script.js
@@ -6,23 +6,40 @@ $(document).ready(function () {
 
 	// link til anden side
 	$("[pageDirect]").css("cursor", "pointer"); //links får en pointer cursor
-	$("[pageDirect]").click(function () {
+	$("[pageDirect]").addClass("pagelink"); // klassen pagelink tilføjes til alle pageDirect for dynamisk at kunne tilføje links
+	$(document).on( "click",".pagelink", function () {
 		$(".page").hide(); //alle sider skjules
 		$("#" + $(this).attr("pageDirect")).show();
 		// navnet på den ID som er i attributen "pageDirect" vises
 		// "pageDirect" fungerer som et link til et ID af en ".page"
 	});
+	
 
 	// post generator
 	// t: overskrift c: kategori a: adresse ti: tid
-	function createListEl(t, c, a, ti, date) {
-		$("#postlist .post-desc, #profile-posts .post-desc").after("<div class='row new-post-row'><div class='col-2-sm'>" + t + "</div><div class='col-3-sm'>" + c + "</div><div class='col-3-sm'>" + a + "</div><div class='col-2-sm'>" + ti + "</div><div class='col-2-sm'>" + date + "</div></div></div>");
+	function createListEl(t, c, a, ti, date, d) {
+		$("#postlist .post-desc, #profile-posts .post-desc").after("<div pageDirect='getterview' class='row new-post-row pagelink'><div class='col-2-sm list-el-title'>" + t + "</div><div class='col-3-sm list-el-category'>" + c + "</div><div class='col-3-sm list-el-adress'>" + a + "</div><div class='col-2-sm list-el-time'>" + ti + "</div><div class='col-2-sm list-el-date'>" + date + "</div><div class='list-el-description hide'>" + d + "</div></div></div>");
 	}
-
 	createListEl("Tømrer", "handyman", "gadenavn 24", "12:00", "29/4");
 	createListEl("Rødvin", "Mad og drikke", "vejnavn 48", "21:00", "17/5");
 
-
+	//gå til opslag
+	//insætter rigtige variabler i opslag
+	//variabler samme som createListEl()	
+	$(document).on( "click",".new-post-row", function () {
+		var t = $(this).find(".list-el-title").text();
+		var c = $(this).find(".list-el-category").text();
+		var a = $(this).find(".list-el-adress").text();
+		var ti = $(this).find(".list-el-time").text();
+		var date = $(this).find(".list-el-date").text();
+		var d = $(this).find(".list-el-description").text();
+		$("#post-title").text(t);
+		$("#post-category p .post-insert").text(c);
+		$("#post-adress p span.post-insert").text(a);
+		$("#post-time p .post-insert").text(ti);
+		$("#post-date p .post-insert").text(date);
+		$("#post-description p .post-insert").text(d);
+	});
 	
 
 
@@ -68,10 +85,11 @@ $(document).ready(function () {
 		var c = $('#form-category').val();
 		var a = $('#form-adress').val();
 		var ti = $('#new-time').val();
+		var d = $('#form-description').val();
 		var date = new Date(); // ny dato
 		var h = date.getDate(); // få dato
 		var m = date.getMonth(); // måned
-		createListEl(t, c, a, ti, h + "/" + m); // der indsættes en post med createListEL()
+		createListEl(t, c, a, ti, h + "/" + m, d); // der indsættes en post med createListEL()
 		$('#form-title').val(""); // formular ryddes efter opslag er udført
 		$('#form-category').val("");
 		$('#form-adress').val("");
